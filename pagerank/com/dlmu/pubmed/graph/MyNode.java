3e6917d269ec1ce3796a22bcf711b5948a6935df
scjensen <scott.jensen@sjsu.edu>
2014-10-11 19:47:48 -0700

    added constructor, reset method, and getmethod

--- pagerank/com/dlmu/pubmed/graph/MyNode.java
+++ pagerank/com/dlmu/pubmed/graph/MyNode.java
@@ -4,17 +4,29 @@ import java.io.Serializable;
 
 public class MyNode implements Serializable{
 	String id; // good coding practice would have this as private
-	double node_prior;
+	double node_prior = 0.0;
 
 	public MyNode(String id, double node_prior) {
 		this.id = id;
 		this.node_prior = node_prior;
 	}
-
+	
+	public MyNode(String id) {
+		this.id = id;
+		node_prior = 0.0;
+	}
+	
+	public void reset() {
+		node_prior = 0.0;
+	} //end of reset
+	
 	public String toString() { // Always a good idea for debuging
 		return id; // JUNG2 makes good use of these.
 	}
 	public void setPrior(double prior){
 		this.node_prior=prior;
+	}
+	public double getPrior() {
+		return(node_prior);
 	}
 }
