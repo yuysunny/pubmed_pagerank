55a1fd0c4da11a5f2fc98471475b7067d475f257
scjensen <scott.jensen@sjsu.edu>
2014-10-11 19:45:39 -0700

    made this the worker class for multi-threading version

--- pagerank/com/dlmu/pubmed/graph/MeshPaperPageRank.java
+++ pagerank/com/dlmu/pubmed/graph/MeshPaperPageRank.java
@@ -1,223 +1,138 @@
 package com.dlmu.pubmed.graph;
 
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.File;
-import java.io.FileInputStream;
 import java.io.FileWriter;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.ObjectInputStream;
 import java.io.PrintWriter;
 import java.io.Serializable;
 import java.util.Collection;
-import java.util.HashMap;
-import java.util.Iterator;
-import java.util.Map;
+
+
+import org.apache.commons.collections15.Transformer;
+import org.apache.log4j.Logger;
 
 import edu.uci.ics.jung.algorithms.scoring.PageRankWithPriors;
-import edu.uci.ics.jung.graph.Graph;
-import edu.uci.ics.jung.graph.SparseMultigraph;
-import edu.uci.ics.jung.graph.util.EdgeType;
 
-public class MeshPaperPageRank {
-//	public static String filePath = "/Users/yyy/Documents/workspace/pubmed/mesh_output/paperPageRank/";
-	public static String filePath ="";
+public class MeshPaperPageRank implements Runnable {
+	// Log4J Logger object for logging in this class
+	protected static Logger log = Logger.getLogger(MeshPaperPageRank.class.getName());
+
+	private static double TOLERANCE = 0.001; 
+	private static int MAX_ITERATIONS = 500;
+	private static double ALPHA = 0.15;
 	public static PrintWriter out2;
 	public static double EDGE_PARA=0.1;
-	public static BufferedWriter bufOut;
-	public static Graph<MyNode, MyLink> g = new SparseMultigraph<MyNode, MyLink>();
-	public static void main(String[] args) throws Exception {
-		filePath=System.getProperty("user.dir")+"/paperPageRank/";
-		System.out.println("loading Nodes file......");
-		long startTime=System.currentTimeMillis();
-		loadingNodes(filePath+"pubmed_id.csv");
-		long endTime=System.currentTimeMillis();   
-		System.out.println("\r\n"+ "loading node time :"+(endTime-startTime)+"ms"); 
-		System.out.println("loading Edges file......");
-		startTime=System.currentTimeMillis();
-		loadingEdges( filePath+"citation_new.csv");
-		endTime=System.currentTimeMillis();  
-		System.out.println("\r\n"+ "loading edge time :"+(endTime-startTime)+"ms"); 
-		System.out.println("start ......");
-		bufOut = new BufferedWriter(new FileWriter(filePath+"log.txt"));
-		runPageRank();
-		
-
-	}
+	private ProcessPaperPageRank  manager = null; //the manager who holds the work queue
+	int workerId = -1;  //set when the constructor is called
+	private PubMedGraph g = null;
+	private boolean initialized = false; //flag that everything was setup OK
+	private boolean shutdown = false;  //should this worker be shutdown
 	
-	public static void runPageRank() throws Exception
-	{
-		File file = new File(filePath+"PaperRelevanceInTopic/");
-		String[] filelist = file.list();
-		for ( int j = 0; j <filelist.length ; j++) 
-		{
-			if(filelist[j].contains("keyword"))
-			{
-				long startTime=System.currentTimeMillis();
-//				System.out.println("..."+filelist[j]);
-				bufOut = new BufferedWriter(new FileWriter(filePath+"log.txt",true));
-				bufOut.write("\r\n"+"page rank j="+j+" filename=  "+filelist[j]);
-				String s[]=filelist[j].split("d_");
-				loadingNodesScore(filePath+"PaperRelevanceInTopic/"+filelist[j]);
-//				System.out.println("The graph g = " + g.toString());
-				pageRankPrior(filePath+"PaperContributeToTopic/",s[1],g);
-				long endTime=System.currentTimeMillis(); //获取结束时间  
-				bufOut.write("\r\n"+ "execute time :"+(endTime-startTime)+"ms"); 
-				bufOut.flush();
-				bufOut.close();
-			}
+	private static Transformer<MyNode, Double> vertex_priors = new Transformer<MyNode, Double>() {
+		public Double transform(MyNode node) {
+			return node.node_prior;
 		}
-		
-		
-	}
-	private static void pageRankPrior(String path,String kid,Graph<MyNode, MyLink> g ) throws IOException{
-		PrintWriter out1 = new PrintWriter(new FileWriter(path+"keyword_"+kid));
-		// primitives
-		// Two disconnected community on graph
-		// Let's see what we have. Note the nice output from the
-		// SparseMultigraph<V,E> toString() method
-		
-		Collection<MyNode> vertices = g.getVertices();
-		// Pagerank with priors
-		// Node Priors
-		org.apache.commons.collections15.Transformer<MyNode, Double> vertex_priors = new org.apache.commons.collections15.Transformer<MyNode, Double>() {
-			public Double transform(MyNode node) {
-				return node.node_prior;
-			}
-		};
-		// Edge Priors
-//		org.apache.commons.collections15.Transformer<MyLink, Double> edge_priors = new org.apache.commons.collections15.Transformer<MyLink, Double>() {
-//			public Double transform(MyLink edge) {
-//				return edge.weight;
-//			}
-//		};
-		PageRankWithPriors ranker_priors = new PageRankWithPriors(g,vertex_priors, 0.15);
-		ranker_priors.setMaxIterations(10);
-		for (int i = 0; i < 500; i++) {
-			ranker_priors.step();
-		}
-		for (MyNode vertex : vertices) {
-			double result=Double.valueOf( ranker_priors.getVertexScore(vertex).toString());
-			
-		
-//			System.out.println("The ranker_priors score of " + vertex + " is: "
-//					+result );
-			out1.println( vertex + ","+ result);
-			out1.flush();
-		}	
-		out1.close();
-//		System.out.println(" out put finish:"+path);
-	}
+	};
 	
-	
-	
-	public static void loadingNodesScore(String path_loading_name ) throws Exception{
-		System.out.println("load node score: "+path_loading_name);
-		Iterator nodes=g.getVertices().iterator();
-		while(nodes.hasNext()){
-		    MyNode myNode=(MyNode)nodes.next();
-		    myNode.setPrior(0d);
-		}
-		BufferedReader bin = new BufferedReader(new InputStreamReader(
-				new FileInputStream(path_loading_name)));
-		String readline;
-		Double sum=0.0;
-		while ((readline = bin.readLine()) != null) {
-			// articleId, type, sorces[]
-			String linestring[] = readline.split(",");
-			double score = Double.valueOf(linestring[1]);
-			sum+=score;
-		}
-		System.out.println("load node score= "+sum);
-		bin.close();
-		bin = new BufferedReader(new InputStreamReader(
-				new FileInputStream(path_loading_name)));
-		while ((readline = bin.readLine()) != null) {
-			// articleId, type, sorces[]
-			String linestring[] = readline.split(",");
-			double score = Double.valueOf(linestring[1]);
-			MyNode n=getNode(linestring[0]);
-			if(n!=null)
-			{
-				n.setPrior(score/sum);
-			}
-			
-		}
-		bin.close();
-//		for(MyNode node:g.getVertices())
-//		{
-//			if(node.node_prior>0)
-//			{
-//				bufOut = new BufferedWriter(new FileWriter(filePath+"log.txt",true));
-//				 bufOut.write("\r\n"+node.id + "-----" + node.node_prior);
-//				 bufOut.close();
-//			}
-//		}
-
-	}
-
-	public static void loadingEdges(String path_loading_name) throws Exception {
+	/**
+	 * The directory for all of the data files and output files can be passed as a parameter
+	 * in the call to the main routine, or the default directory within the working directory
+	 * will be used if null is passed as the parameter.
+	 * Data files expected within that directory:
+	 * The nodes file (default is pubmed_id.csv)
+	 * The edges file (default is citation_new.csv)
+	 * The weights for the papers given a keyword.  Each keyword is
+	 * in a separate file with the MeSH descriptor and qualifier in the file name.
+	 * These files are expected in a subdirectory named PaperRelevanceInTopic.
+	 * @param args
+	 */
+	public MeshPaperPageRank(int workerId, ProcessPaperPageRank manager, String nodeFileName, String edgeFileName) {
 		try {
-			BufferedReader bin = new BufferedReader(new InputStreamReader(
-					new FileInputStream(path_loading_name)));
-			String readline;
-			int i = 0;
-			// out2 = new PrintWriter(new
-			// FileWriter(filePath+"acm200truePaperCitePaper.csv"));
-			while ((readline = bin.readLine()) != null) {
-				if (!readline.contains("id")) {
-					String linestring[] = readline.split(",");
-
-					MyNode beginNode = getNode(linestring[0]);
-					MyNode endNode = getNode(linestring[1]);
-					if (beginNode != null && endNode != null) {
-						MyLink edge = new MyLink(i, 0);
-						g.addEdge(edge, beginNode, endNode, EdgeType.DIRECTED);
-						
-						// out2.flush();
-						i++;
-					}
-				}
-				
-			}
-			bin.close();
-			// out2.close();
+			this.workerId = workerId;
+			this.manager = manager;
+			g = new PubMedGraph(nodeFileName, edgeFileName);
+			initialized = g.isInitialized();
+			return;
 		} catch (Exception e) {
-			System.out.println("loading error");
-			e.printStackTrace();
+			initialized = false;
+			log.error("MeshPaperPageRank-constructor: an exception occurred in setting up the MeshPaperPageRank: " +
+					e.getMessage(), e);
+			try {g.clear();} catch (Exception ex){}
 		}
-
-	}
-
-	public static void loadingNodes(String path_loading_name) throws Exception {
-		HashMap keyScore = new HashMap();
-		BufferedReader bin = new BufferedReader(new InputStreamReader(
-				new FileInputStream(path_loading_name)));
-		String readline;
-		while ((readline = bin.readLine()) != null) {
-			// articleId, type, sorces[]
-			if (!readline.contains("id")) {
-				double score = 0.0;
-				MyNode node1 = new MyNode(readline, score);
-//				 System.out.println(node1.id + "-----" + node1.node_prior );
-				g.addVertex(node1);
-			}
-
-		}
-		bin.close();
-	}
-
-	public static MyNode getNode(String nodeId) {
-		Iterator nodes = g.getVertices().iterator();
-		while (nodes.hasNext()) {
-			MyNode myNode = (MyNode) nodes.next();
-			if (nodeId.hashCode() == myNode.id.hashCode()) {
-				return myNode;
-			}
-		}
-		return null;
-	}
+	} //end of constructor
 	
+	public boolean isInitialized() {
+		return initialized;
+	} //end of isInitialized
+	
+	public void shutdown() {
+		log.info("Shutdown called for worker: " + workerId);
+		shutdown = true;
+	} //end of shutdown
+	
+	
+
+	public void run() {
+		PageRankJob job = null; //job being processed
+		String keyword = null;
+		
+		try {Thread.sleep(2000);} catch (InterruptedException e){}
+		
+		
+		try {
+			while(!shutdown) {
+				job = manager.getJob();
+				if (job == null)
+					shutdown = true;
+				else { //process the job
+					keyword = job.getKeyword();
+					long startTime=System.currentTimeMillis();
+					g.loadNodesScore(job.getNodeFileName());
+					long nodeScoreTime = System.currentTimeMillis();
+					pageRankPrior(job.getOutputFileName(), keyword, g);
+					long endTime=System.currentTimeMillis();
+					if (log.isInfoEnabled() )
+						log.info("MeshPaperPageRank-runPageRank: for keyword " +
+								keyword + " the loading of nodes took " +
+								(nodeScoreTime - startTime) +"ms and pagerank took " +
+								(endTime - nodeScoreTime) +"ms");
+				}
+			} //end of while not shutdown loop
+		} catch (Exception e) {
+			log.error("An exception was caught when running pagerank for " +
+					keyword + ". Exception: " + e.getMessage(), e);
+		} finally {
+			try {g.clear();} catch(Exception e){}
+		}
+	} //end of runPageRank
+	
+	
+	private void pageRankPrior(String path, String kid,PubMedGraph g) throws Exception {
+		PrintWriter out = null;
+		try {
+			PageRankWithPriors<MyNode, MyLink> rankerWithPriors = new PageRankWithPriors<MyNode, MyLink>(g, vertex_priors, ALPHA);
+			rankerWithPriors.setMaxIterations(MAX_ITERATIONS);
+			rankerWithPriors.setTolerance(TOLERANCE);
+			rankerWithPriors.evaluate();
+			if(log.isDebugEnabled()) {
+				int iterations = rankerWithPriors.getIterations();
+				log.debug("MeshPaperPageRank-pageRankPrior: iterations for keyword: " + 
+						kid + " = " + iterations);
+			}
+			out = new PrintWriter(new FileWriter(path));
+			Collection<MyNode> nodes = g.getVertices();
+			
+			for (MyNode node: nodes) {
+				double score = rankerWithPriors.getVertexScore(node);
+				if (score > 0.0)
+					out.println( node + ","+ score);
+			}
+			return;
+		} catch (Exception e) {
+			throw new Exception("An exception occurred in pageRankPrior for keyword: " + 
+					kid + ". Exception: " + e.getMessage(), e);
+		} finally {
+			try{out.flush();} catch (Exception ex){}
+			try{out.close();} catch (Exception ex){}
+		}
+	} //end of pageRankPrior
 }
 
